d65b5ab84332019c2982d6b15ffc79db440dc8ca
Dmitriy Sinelnikov <dmitriy.sinelnikoff@gmail.com>
2016-03-04 15:48:35 +0600

    Last version of coder.

---
+++ CDC/CDC/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        public static string Coding (string a, int key)
+        {
+            string ResStr = ""; //Resulted String
+            int SL = a.Length; //String Length
+            for (int i=0; i < SL; i++)
+            {
+                ResStr = ResStr.Insert(ResStr.Length, Convert.ToString((char)(a[i] ^ key)));
+            }
+            return ResStr;
+        }
+
+        static void Main(string[] args)
+        {
+            string EntStr;
+            Console.WriteLine("Enter string to code:");
+            EntStr = Console.ReadLine();
+            Console.WriteLine("Enter code key:");
+            int CK = int.Parse(Console.ReadLine()); ; // Coding Key
+            string CdS = Coding(EntStr, CK);
+            Console.WriteLine("Coded string");
+            Console.WriteLine(CdS);
+            EntStr = Coding(CdS, CK);
+            Console.WriteLine("Decoded string");
+            Console.WriteLine(EntStr);
+            Console.ReadKey();
+        }
+    }
+}
--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-	echo "NUJEN KINGLIKT";
-	echo "Changes for commit 2";
-
-?>
